5129b74754f0d74cd2af652cb09024ba734a7371
zpremio <svenrus@live.de>
2014-11-28 22:23:58 +0100

    Erstellt
    Repository erstellt.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+namespace Adressbuch
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool bconn = true;
+            while (bconn == true)
+            {
+                try
+                {
+                    OleDbConnection conn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=..\\..\\Adressbuch2.mdb");
+                    conn.Open();
+                    Console.WriteLine("Verbindung wird versucht hergestellt...");
+                    bconn = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Verbindung konnte NICHT hergestellt...");
+                }
+            }
+            Console.WriteLine("...");
+            Console.WriteLine("Die Verbindung zur Datenbank wurde erfolgreich hergestellt.");
+            Console.WriteLine("");
+            Console.WriteLine("Was möchtest du tun?");
+            Console.WriteLine("");
+            Console.WriteLine("(1) Datensätze anzeigen");
+            Console.WriteLine("(2) Datensatz hinzufügen");
+            Console.WriteLine("(3) Datensatz löschen");
+
+            Console.ReadLine();
+        }
+    }
+}
+
